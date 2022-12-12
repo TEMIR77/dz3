4dd09bc0ca9e897c106a2dc6b48c91f70b6ac962
TEMIR77 <turmatbekov77@gmail.com>
2022-12-12 17:30:22 +0600

    dz3

---
+++ main.js
@@ -0,0 +1,30 @@
+// function min(num1, num2) {
+//     if(num1 < num2){
+//         return num1
+//     }else {
+//         return num2
+//     }
+// }
+// console.log(min(77,33));
+
+// let massiv1=[5,7,8,9,7]
+// let massiv2=[3,4,2,7,1,0]
+
+// function twoMass(){
+//     if(massiv1.length> massiv2.length){
+//         console.log("massiv1 длиннее");
+//     }else if(massiv2.length > massiv1.length){
+//         console.log("massiv2 длиннее");
+//     }
+    
+    
+// }
+// console.log(twoMass());
+
+
+let js1 = 'string777'
+let res = js1.length
+function countChar(arg1){
+    return arg1.length
+}
+console.log(countChar(js1));
